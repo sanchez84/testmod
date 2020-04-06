e701106764332e9cf1ea44244ca8c4fbe6868abe
sanchez84 <hahol14@gmail.com>
2020-04-06 15:11:50 +0500

    Commit v1.0.0

--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package testmod
 
 import "fmt"
 
-// Hi - Say Hi <name>
-func Hi(name string) {
-	fmt.Printf("Hi %s\n", name)
-}
-
-// Hi2 - Say Hi <name>, <lastName>
-func Hi2(name string, lastName string) {
-	fmt.Printf("Hello %s, %s\n", name, lastName)
+// Hello - Say Hello <name>
+func Hello(name string) {
+	fmt.Printf("Hello %s\n", name)
 }
